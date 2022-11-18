1b841bfc0c8fbf16a793ea185691297a5dda4bd4
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2022-11-18 07:13:21 +0100

    After lab6

--- lista6/zad4/main.js
+++ lista6/zad4/main.js
@@ -11,61 +11,73 @@ box.addEventListener("mouseout", onMouseOutBox);
 var output = document.getElementById("output");
 
 
-function onMouseMove(e) {
+function onMouseMove(e)
+{
     output.innerHTML = "Mouse move";
     restOfData(e);
 
 }
 
-function onMouseDown(e) {
+function onMouseDown(e)
+{
     output.innerHTML = "Mouse down";
     restOfData(e);
 
 }
 
-function onMouseOver(e) {
+function onMouseOver(e)
+{
     output.innerHTML = "Mouse over";
     restOfData(e);
 
 }
 
-function onMouseOut(e) {
+function onMouseOut(e)
+{
     output.innerHTML = "Mouse out";
     restOfData(e);
 }
 
-function onKeyPress(e) {
+function onKeyPress(e)
+{
     output.innerHTML = "Key pressed";
     restOfData(e);
 }
 
-function onKeyDown(e) {
+function onKeyDown(e)
+{
     output.innerHTML = "Key down";
     restOfData(e);
 }
 
-function onMouseOverBox(e) {
+function onMouseOverBox(e)
+{
     box.innerHTML = "Mouse over box";
 }
 
-function onMouseOutBox(e) {
+function onMouseOutBox(e)
+{
     box.innerHTML = "Mouse out box";
 }
 
 
-function restOfData(e){
-    if(e.altKey!=undefined){
-    output.innerHTML += "<br>Alt key: " + e.altKey + "<br>";
-    output.innerHTML += "Ctrl key: " + e.ctrlKey + "<br>";
-    output.innerHTML += "Shift key: " + e.shiftKey  + "<br>";
+function restOfData(e)
+{
+    if (e.altKey != undefined)
+    {
+        output.innerHTML += "<br>Alt key: " + e.altKey + "<br>";
+        output.innerHTML += "Ctrl key: " + e.ctrlKey + "<br>";
+        output.innerHTML += "Shift key: " + e.shiftKey + "<br>";
     }
-    if(e.keyCode!=undefined){
-    output.innerHTML += "Key code: " + e.keyCode + "<br>";
+    if (e.keyCode != undefined)
+    {
+        output.innerHTML += "Key code: " + e.keyCode + "<br>";
     }
-    if(e.clientX!=undefined){
-    output.innerHTML += "Client x: " + e.clientX + "<br>";
-    output.innerHTML += "Client y: " + e.clientY + "<br>";
-    output.innerHTML += "Screen x: " + e.screenX + "<br>";
-    output.innerHTML += "Screen y: " + e.screenY + "<br>";
+    if (e.clientX != undefined)
+    {
+        output.innerHTML += "Client x: " + e.clientX + "<br>";
+        output.innerHTML += "Client y: " + e.clientY + "<br>";
+        output.innerHTML += "Screen x: " + e.screenX + "<br>";
+        output.innerHTML += "Screen y: " + e.screenY + "<br>";
     }
 }
--- lista6/zad5/main.js
+++ lista6/zad5/main.js
@@ -2,28 +2,34 @@
 document.getElementById("reset").addEventListener("click", confirmReset);
 document.getElementById("submit").addEventListener("click", confirmSubmit);
 
-function confirmReset() {
+function confirmReset()
+{
     return confirm("Are you sure you want to reset the form?");
 }
 
-function confirmSubmit() {
+function confirmSubmit()
+{
     return confirm("Are you sure you want to submit the form?");
 }
 
 
 //add focus and blur event to all input elements, this will be used to to show help text
 var inputs = document.getElementsByTagName("input");
-for (var i = 0; i < inputs.length; i++) {
+for (var i = 0; i < inputs.length; i++)
+{
     inputs[i].addEventListener("focus", showHelp);
     inputs[i].addEventListener("blur", hideHelp);
 }
 
-function showHelp() {
+function showHelp()
+{
     var helpText = this.nextElementSibling;
     helpText.style.display = "inline";
 }
 
-function hideHelp() {
+function hideHelp()
+{
     var helpText = this.nextElementSibling;
     helpText.style.display = "none";
+    x = 12;
 }

5f30f36b9699d6f4fd755eb6e7f21e3805cafa50
Tomasz Płóciennik SQ3ESP <83788346+sq3esp@users.noreply.github.com>
2023-01-17 00:40:01 +0100

    Po zajęciach

--- lista12/StudentListFragments/StudentListFragments/src/main/java/com/example/productlistfragments/CategoryController.java
+++ lista12/StudentListFragments/StudentListFragments/src/main/java/com/example/productlistfragments/CategoryController.java
@@ -12,9 +12,11 @@ import java.util.Locale;
 @Controller
 public class CategoryController {
     private final CategoryService categoryService;
+    private final ProductService productService;
 
-    public CategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService, ProductService productService) {
         this.categoryService = categoryService;
+        this.productService = productService;
     }
 
     @GetMapping("/category")
@@ -23,17 +25,15 @@ public class CategoryController {
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
                 DateFormat.LONG, locale);
         String serverTime = dateFormat.format(date);
-        model.addAttribute("serverTime", serverTime.toString() );
+        model.addAttribute("serverTime", serverTime.toString());
         List<Category> categoryList = categoryService.getAllCategory();
-        model.addAttribute("categoryList", categoryList );
+        model.addAttribute("categoryList", categoryList);
         return "product/index";
     }
 
-
-
     @GetMapping("/product/addCategory")
     public String add(Model model) {
-        model.addAttribute("category", new Category() );
+        model.addAttribute("category", new Category());
         return "product/addCategory";
     }
 
@@ -43,17 +43,15 @@ public class CategoryController {
         return "redirect:/product/";
     }
 
-
-
     // how to put parameter in an URL
     // e.a. /student/3/edit
-    @GetMapping(value = {"/product/{studId}/editCategory"})
+    @GetMapping(value = { "/product/{studId}/editCategory" })
     public String edit(Model model, @PathVariable Integer studId) {
-        model.addAttribute("category", categoryService.getCategoryById(studId) );
+        model.addAttribute("category", categoryService.getCategoryById(studId));
         return "/product/editCategory";
     }
 
-    @PostMapping(value = {"/product/editCategory"})
+    @PostMapping(value = { "/product/editCategory" })
     public String edit(@ModelAttribute Category category) {
         categoryService.updateCategory(category);
         return "redirect:/product/";
@@ -63,14 +61,13 @@ public class CategoryController {
     // e.a. /student/remove?id=3
     @GetMapping("/category/remove")
     public String remove(@RequestParam("id") long id) {
-        List<Product> productList = categoryService.getAllProductByCategoryId(id);
+        List<Product> productList = productService.getAllProduct();
         for (Product product : productList) {
-            categoryService.deleteProductById(product.getId());
+            if (product.getCategory() == id)
+                productService.deleteProductById(product.getId());
         }
         categoryService.deleteCategoryById(id);
         return "redirect:/product/";
     }
 
 }
-
-
--- lista12/StudentListFragments/StudentListFragments/src/main/java/com/example/productlistfragments/ProductController.java
+++ lista12/StudentListFragments/StudentListFragments/src/main/java/com/example/productlistfragments/ProductController.java
@@ -14,7 +14,6 @@ public class ProductController {
     private final ProductService productService;
     private final CategoryService categoryService;
 
-
     public ProductController(ProductService productService, CategoryService categoryService) {
         this.productService = productService;
         this.categoryService = categoryService;
@@ -26,51 +25,49 @@ public class ProductController {
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
                 DateFormat.LONG, locale);
         String serverTime = dateFormat.format(date);
-        model.addAttribute("serverTime", serverTime.toString() );
+        model.addAttribute("serverTime", serverTime.toString());
         List<Product> productList = productService.getAllProduct();
-        model.addAttribute("productList", productList );
+        model.addAttribute("productList", productList);
         List<Category> categoryList = categoryService.getAllCategory();
-        model.addAttribute("categoryList", categoryList );
+        model.addAttribute("categoryList", categoryList);
         return "product/index";
     }
 
-
-
     @GetMapping("/product/add")
     public String add(Model model) {
-        //pass category list to the view
+        // pass category list to the view
         List<Category> categoryList = categoryService.getAllCategory();
-        model.addAttribute("category", categoryList );
-        model.addAttribute("product", new Product() );
+        model.addAttribute("categories", categoryList);
+        model.addAttribute("product", new Product());
         return "product/add";
     }
 
     @PostMapping("/product/add")
     public String add(@ModelAttribute Product product) {
+
         productService.addProduct(product);
         return "redirect:/product/";
     }
-
 
     // how to put a parameter in a query string
     // e.a. /student/details?id=3
     @GetMapping("/product/details")
     public String add(@RequestParam("id") long inputId, Model model) {
-        model.addAttribute("product", productService.getProductById(inputId) );
+        model.addAttribute("product", productService.getProductById(inputId));
         return "/product/details";
     }
 
     // how to put parameter in an URL
     // e.a. /student/3/edit
-    @GetMapping(value = {"/product/{studId}/edit"})
+    @GetMapping(value = { "/product/{studId}/edit" })
     public String edit(Model model, @PathVariable Integer studId) {
         List<Category> categoryList = categoryService.getAllCategory();
-        model.addAttribute("category", categoryList );
-        model.addAttribute("product", productService.getProductById(studId) );
+        model.addAttribute("categories", categoryList);
+        model.addAttribute("product", productService.getProductById(studId));
         return "/product/edit";
     }
 
-    @PostMapping(value = {"/product/edit"})
+    @PostMapping(value = { "/product/edit" })
     public String edit(@ModelAttribute Product product) {
         productService.updateProduct(product);
         return "redirect:/product/";
@@ -84,7 +81,4 @@ public class ProductController {
         return "redirect:/product/";
     }
 
-
 }
-
-
